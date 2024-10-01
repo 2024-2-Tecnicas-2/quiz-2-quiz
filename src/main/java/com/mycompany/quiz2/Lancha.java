be03f0bfea8afa0a01538cd5588e01da500903e1
jhondoe <jhondoe@gmail.com>
2024-09-30 20:36:10 -0500

    Clase Lancha

---
+++ src/main/java/com/mycompany/quiz2/Lancha.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quiz2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Lancha extends VehiculoBase {
+
+    public Lancha(String marca, String modelo, int kilometraje) {
+        super(marca, modelo, kilometraje);
+    }
+
+    @Override
+    public void Acelerar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Frenar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Combustible() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
